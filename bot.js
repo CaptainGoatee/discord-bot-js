f0c0d904ad226e78edcbbb4b983d49a729daf290
CaptainGoatee <121584207+CaptainGoatee@users.noreply.github.com>
2026-01-03 06:42:24 +0000

    added comments

--- bot.js
+++ bot.js
@@ -14,6 +14,19 @@ new DiscordHandler({
   validationsPath: "./validations",
   testServer: process.env.DISCORD_TEST_GUILD_ID,
   logger: Logger,
-  refreshCommands: true,
-  toggleCommands: true,
+  refreshCommands: false, // Adds a command called /app refreshCommands to refresh the commands to the Discord API
+  toggleCommands: false, // Adds a command called /app enable/disable <command> to enable a previously disabled command
 });
+
+// This is the main file for the bot. It handles creating commands, logging in, and handling events for you.
+
+
+// `/app refresh_commands`
+// Reloads all commands and functions. Useful for development.
+
+// `/app enable <command>`
+// Enables a previously disabled command.
+
+// `/app disable <command>`
+// Disables a command (resets when the bot is restarted).
+
