b1464b3e4c8d10586d1f4d1631633ee25578a862
CaptainGoatee <121584207+CaptainGoatee@users.noreply.github.com>
2026-01-03 06:42:39 +0000

    addded comments

--- events/ready.js
+++ events/ready.js
@@ -3,3 +3,5 @@ const { Logger } = require("term-logger");
 module.exports = (event) => {
   Logger.success(`Logged in as ${event.client.user.tag}`);
 };
+
+// This event is triggered when the bot is ready
